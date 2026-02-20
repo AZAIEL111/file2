ffc90b564a2a1efe9ce3eac41926cf0f2cfe0671
MR.A <masteralpha069@gmail.com>
2026-02-20 21:09:48 +0500

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+arr=np.array([2,4,6,8])
+print(arr)
+
+
+arr=np.linspace(20,40,80)
+print(arr)
+
+arr=np.random.randint(4,10,40)
+print(arr)
+
+
+arr=np.array([2,5,8,10])
+print(arr.shape)
+print(arr.ndim)
+print(arr.size)
+print(arr.dtype)
+
+
+arr=np.array([2,5,7,9,13])
+print(arr[1:4])
+
+arr=np.array([2,4,7,11,13,17])
+print(arr.dtype)
+new=arr.astype(float)
+print(new)
+print(new.dtype)
+
+
+
+import numpy as np 
+arr = np.array([1, 2, 3]) 
+arr + 5 
+
+
+import numpy
+arr=np.array([1,2,3])
+print(arr*3)
+
+
+
+list=[1,2,3,4]
+print(list *3)
+
+
+import numpy as np
+print(np.ones(2))
+print(np.arange(2,20,3))
+print(np.linspace(0,10,20))
+print(np.random.randint(1,100,5))
+
+
