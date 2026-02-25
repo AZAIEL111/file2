b2eff252b00bb2b735f67c90aa86ed48bb5be232
MR.A <masteralpha069@gmail.com>
2026-02-25 13:16:57 +0500

    Add files via upload

---
+++ file5.py
@@ -0,0 +1,19 @@
+width=int(input("enter the width "))
+height=int(input("enter the height "))
+# print("result",width*height)
+
+
+
+
+arr=["ahmad",18,10.5,"bscs"]
+for i in arr:
+    print(i)
+
+
+while True:
+    user=(input("enter ur name"))
+    if len(user)<4:
+        print()
+    else:
+        print("name should be less than four char")
+        
